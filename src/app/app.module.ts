0efa4a54a2fdb075d80c28158116fd0f542678c9
Mizraim Martínez <mizraim.martinez@gmail.com>
2023-01-14 17:06:20 -0600

    creacion de componentes y estilos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ContentComponent } from './components/content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    SidebarComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule
